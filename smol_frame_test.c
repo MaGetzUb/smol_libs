f668d6f0dd524c473b1765d0eb27bdaabb2b658e
Marko <marko_ranta@protonmail.com>
2023-06-05 05:43:27 +0300

    Removed old code residue, and utilized the new input interface.

--- smol_frame_test.c
+++ smol_frame_test.c
@@ -7,6 +7,9 @@
 #define SMOL_FRAME_IMPLEMENTATION
 #include "smol_frame.h"
 
+#define SMOL_INPUT_IMPLEMENTATION
+#include "smol_input.h"
+
 #define SMOL_UTILS_IMPLEMENTATION
 #include "smol_utils.h"
 
@@ -27,16 +30,10 @@ int main(int numArgs, const char* argv[]) {
 
 	smol_frame_set_title(frame, "Hello, world!");
 
-#if 0
-	for(Uint32 j = 0U; j < 600U; j++)
-	for(Uint32 i = 0u; i < 800u; i++)
-		surface[i + j * 800U] = (i & 255) ^ (j & 255);
-#endif 
 
 	Olivec_Canvas canvas = olivec_canvas(surface, 800, 600, 800);
 
 	bool running = true;
-	bool drawing = false;
 
 	int mouseX = 0;
 	int mouseY = 0;
@@ -54,6 +51,7 @@ int main(int numArgs, const char* argv[]) {
 		double tp1 = smol_timer();
 
 		smol_frame_update(frame);
+		smol_inputs_flush();
 
 		for(smol_frame_event_t event; smol_frame_acquire_event(frame, &event);) {
 			if(event.type == SMOL_FRAME_EVENT_CLOSED) {
@@ -65,20 +63,18 @@ int main(int numArgs, const char* argv[]) {
 				printf("Frame resolution size is now: (%d x %d)\n", event.size.width, event.size.height);
 				break;
 			}
-			else if(event.type == SMOL_FRAME_EVENT_MOUSE_BUTTON_DOWN) {
-				if(event.mouse.button == 1) drawing = true;
+			else {
+				smol_inputs_update(&event);
+			}
+
+			/*else if(event.type == SMOL_FRAME_EVENT_MOUSE_BUTTON_DOWN) {
 				printf("Mouse button (%d) down event at (%d, %d)\n", event.mouse.button, event.mouse.x, event.mouse.y);
 				break;
 			}
 			else if(event.type == SMOL_FRAME_EVENT_MOUSE_BUTTON_UP) {
-				if(event.mouse.button == 1) drawing = false;
+				printf("Mouse button (%d) up event at (%d, %d)\n", event.mouse.button, event.mouse.x, event.mouse.y);
 			}
 			else if(event.type == SMOL_FRAME_EVENT_MOUSE_MOVE) {
-			#if 0
-				if(drawing) {
-					surface[event.mouse.x + event.mouse.y * 800] |= (255 << 16);
-				}
-			#endif 
 				mouseX = event.mouse.x;
 				mouseY = event.mouse.y;
 			}
@@ -97,15 +93,15 @@ int main(int numArgs, const char* argv[]) {
 			}
 			else if(event.type == SMOL_FRAME_EVENT_FOCUS_GAINED) {
 				puts("Focus gained.");
-			}
+			}*/
 
 		}
 
 		olivec_fill(canvas, 0xFF0000AA);
 
 		olivec_rect(canvas, 180, 150, 75, 30, 0xFFCCAA88);
-		olivec_line(canvas, 400, 300, mouseX, mouseY, 0xFFFF00FF);
-		olivec_circle(canvas, mouseX, mouseY, 10, 0xFF00CC00);
+		olivec_line(canvas, 400, 300, smol_mouse_x(), smol_mouse_y(), 0xFFFF00FF);
+		olivec_circle(canvas, smol_mouse_x(), smol_mouse_y(), 10+smol_mouse_z()*2, 0xFF000000 | (0x00CC00 << (smol_mouse_down(1) * 8)));
 		olivec_text(canvas, "hello, world!", 10, 10, olivec_default_font, 2, 0xFF00FFCC);
 
 		snprintf(buf, 256, "fps: %d", fps);
