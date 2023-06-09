17186599037a81c567d08997ce427f3f2eeb474f
Marko <marko_ranta@protonmail.com>
2023-06-10 02:50:12 +0300

    Added text inputing in the smol_frame_test.c and added also support for utf-8 printing in Win32 console. Added also unicode codepoint -> utf-8 string conversion function, and set locale to LC_ALL

--- smol_frame_test.c
+++ smol_frame_test.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <locale.h>
+#include <ctype.h>
 
-#define SMOL_FRAME_BACKEND_XCB
+//#define SMOL_FRAME_BACKEND_XCB
 #define SMOL_FRAME_IMPLEMENTATION
 #include "smol_frame.h"
 
@@ -29,8 +31,15 @@ typedef unsigned int Uint32;
 
 Uint32 surface[800 * 600];
 
+int utf32_to_utf8(unsigned int chr, int bufLen, char* buf);
+
 
 int main(int numArgs, const char* argv[]) {
+	
+#ifdef SMOL_PLATFORM_WINDOWS
+	SetConsoleOutputCP(CP_UTF8);
+#endif 
+	setlocale(LC_ALL, 0);
 
 	smol_frame_t* frame = smol_frame_create(800, 600, "Smol Frame :)");
 
@@ -54,6 +63,8 @@ int main(int numArgs, const char* argv[]) {
 	char buf[256] = {0};
 
 	printf("%lf\n", smol_timer());
+	char input_buffer[256] = {0};
+	int input_cursor = 0;
 
 	while(running) {
 
@@ -71,6 +82,13 @@ int main(int numArgs, const char* argv[]) {
 			else if(event.type == SMOL_FRAME_EVENT_RESIZE) {
 				printf("Frame resolution size is now: (%d x %d)\n", event.size.width, event.size.height);
 				break;
+			} else if(event.type == SMOL_FRAME_EVENT_TEXT_INPUT) {
+				if(event.unicode < 127 && isprint(event.unicode)) 
+					input_buffer[input_cursor++] = (char)event.unicode;
+			} else if(event.type == SMOL_FRAME_EVENT_KEY_DOWN) {
+				if(event.key.code == SMOLK_BACKSPACE && input_cursor > 0) {
+					input_buffer[--input_cursor] = 0;
+				}
 			}
 			else {
 				smol_inputs_update(&event);
@@ -118,6 +136,9 @@ int main(int numArgs, const char* argv[]) {
 			blue = 255.f;
 		}
 
+		if(smol_key_down(SMOLK_SPACE)) {
+			olivec_rect(canvas, 250, 200, 75, 35, 0xFFCC88AA);
+		}
 
 
 		if(red > 0.f) red -= 100.f * (float)dt; else red = 0.f;
@@ -127,9 +148,13 @@ int main(int numArgs, const char* argv[]) {
 		olivec_rect(canvas, 180, 150, 75, 30, 0xFFCCAA88);
 		olivec_line(canvas, anchorX, anchorY, smol_mouse_x(), smol_mouse_y(), 0xFFFF00FF);
 
-
 		olivec_circle(canvas, smol_mouse_x(), smol_mouse_y(), 10+smol_mouse_z()*2, 0xFF00CC00 | ((int)red) << 16 | (int)blue);
 		olivec_text(canvas, "hello, world!", 10, 10, smol_font, 1, 0xFF00FFCC);
+
+		snprintf(buf, 256, "Input: %s", input_buffer);
+		olivec_text(canvas, buf, 10, 200, smol_font, 1, 0xFF00FFCC);
+
+
 		olivec_text(canvas, "!\"#$%&'()*+,-./0123456789:;<=>?@", 10, 25, smol_font, 1, 0xFFAA00DD);
 		olivec_text(canvas, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 10, 45, smol_font, 1, 0xFFAA00DD);
 		olivec_text(canvas, "[\\]^_`", 10, 65, smol_font, 1, 0xFFAA00DD);
@@ -159,4 +184,60 @@ int main(int numArgs, const char* argv[]) {
 	smol_frame_destroy(frame);
 
 	return 0;
+}
+
+
+int utf32_to_utf8(unsigned int chr, int buf_len, char* buf) {
+
+	int clen = 1;
+	if(chr >= 0x0000080) clen = 2;
+	if(chr >= 0x0000800) clen = 3;
+	if(chr >= 0x0010000) clen = 4;
+	if(chr >= 0x010F800) clen = 5;
+	if(chr >= 0x3FFFFFF) clen = 6;
+
+	if(clen > buf_len) return 0;
+
+	char* buf_byte = buf;
+
+	switch(clen) {
+		case 0:
+		break;
+		case 1:
+			*buf_byte++ = chr & 0xFF;
+		break;
+		case 2:
+			*buf_byte++ = 0xC0 | ((chr >> 6) & 0x1F);
+			*buf_byte++ = 0x80 | (chr & 0x3F);
+		break;
+		case 3:
+			*buf_byte++ = 0xE0 | ((chr >> 12) & 0xF);
+			*buf_byte++ = 0x80 | ((chr >> 6) & 0x3F);
+			*buf_byte++ = 0x80 | (chr & 0x3F);
+		break;
+		case 4:
+			*buf_byte++ = 0xF0 | ((chr >> 18) & 0x7);
+			*buf_byte++ = 0x80 | ((chr >> 12) & 0x3F);
+			*buf_byte++ = 0x80 | ((chr >> 6) & 0x3F);
+			*buf_byte++ = 0x80 | (chr & 0x3F);
+		break;
+		case 5:
+			*buf_byte++ = 0xF8 | ((chr >> 24) & 0x3);
+			*buf_byte++ = 0x80 | ((chr >> 18) & 0x3F);
+			*buf_byte++ = 0x80 | ((chr >> 12) & 0x3F);
+			*buf_byte++ = 0x80 | ((chr >>  6) & 0x3F);
+			*buf_byte++ = 0x80 | (chr & 0x3F);
+		break;
+		case 6:
+			*buf_byte++ = 0xFC | ((chr >> 31) &  0x1);
+			*buf_byte++ = 0x80 | ((chr >> 24) & 0x3F);
+			*buf_byte++ = 0x80 | ((chr >> 18) & 0x3F);
+			*buf_byte++ = 0x80 | ((chr >> 12) & 0x3F);
+			*buf_byte++ = 0x80 | ((chr >>  6) & 0x3F);
+			*buf_byte++ = 0x80 | (chr & 0x3F);
+		break;
+	}
+
+	return clen;
+
 }
