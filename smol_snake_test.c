bad32d4af630b6dd1d0b8701df913d79b1515eef
Marko <marko_ranta@protonmail.com>
2023-06-11 15:47:37 +0300

    Fixed so that player cannot turn into direction snake comes from between move steps. Fixed the occupancy map, so the point is never placed under the snake. Also occupancy is now set after snake is warped, so occupancy map is not written over bounds.

--- smol_snake_test.c
+++ smol_snake_test.c
@@ -39,10 +39,11 @@ static Olivec_Font_ smol_font_ = {
 };
 
 
-#define SNAKE_SIZE 20
+#define SNAKE_SIZE 10
 #define ARENA_W 20
 #define ARENA_H 20
-#define HUD_H 100 //Pixels
+#define HUD_H 40 //Pixels
+#define PIXEL_SCALE 4
 
 #define SCREEN_W (ARENA_W * SNAKE_SIZE)
 #define SCREEN_H (ARENA_H * SNAKE_SIZE + HUD_H)
@@ -63,13 +64,14 @@ OLIVECDEF void olivec_text_(Olivec_Canvas oc, const char* text, int tx, int ty, 
 
 int main(int argc, char* argv[]) {
 
-	smol_frame_t* frame  = smol_frame_create(SCREEN_W, SCREEN_H, "snek");
+	smol_frame_t* frame  = smol_frame_create(SCREEN_W*PIXEL_SCALE, SCREEN_H*PIXEL_SCALE, "snek");
 	Olivec_Canvas canvas = olivec_canvas(pixels, SCREEN_W, SCREEN_H, SCREEN_W);
 
-	char occupied_arena[ARENA_W * ARENA_H];
+	char occupied_arena[ARENA_W * ARENA_H] = {0};
 
 	snek_t snek_pieces[10000];
 	int snek_len = 3;
+	int sdx = 1, sdy = 0;
 	int dx = 1, dy = 0;
 	snek_pieces[0] = (snek_t){ARENA_W / 2, ARENA_H / 2};
 
@@ -80,8 +82,6 @@ int main(int argc, char* argv[]) {
 	}
 	double next_step = SPEED_FORMULA;
 	double move_step = next_step;
-
-	printf("Step speed %lf\n", next_step);
 
 	double time_accum = 0.;
 	double dt = 0., tp1 = 0.;
@@ -106,13 +106,16 @@ int main(int argc, char* argv[]) {
 			smol_inputs_update(&ev);
 		}
 
-		if((smol_key_hit(SMOLK_W) || smol_key_hit(SMOLK_UP)) && dx != 0) dy = -1, dx = 0;
-		if((smol_key_hit(SMOLK_S) || smol_key_hit(SMOLK_DOWN)) && dx != 0) dy = 1, dx = 0;
+		if((smol_key_hit(SMOLK_W) || smol_key_hit(SMOLK_UP)) && sdx != 0) dy = -1, dx = 0;
+		if((smol_key_hit(SMOLK_S) || smol_key_hit(SMOLK_DOWN)) && sdx != 0) dy = 1, dx = 0;
 
-		if((smol_key_hit(SMOLK_D) || smol_key_hit(SMOLK_RIGHT)) && dy != 0) dy = 0, dx = 1;
-		if((smol_key_hit(SMOLK_A) || smol_key_hit(SMOLK_LEFT)) && dy != 0) dy = 0, dx = -1;
+		if((smol_key_hit(SMOLK_D) || smol_key_hit(SMOLK_RIGHT)) && sdy != 0) dy = 0, dx = 1;
+		if((smol_key_hit(SMOLK_A) || smol_key_hit(SMOLK_LEFT)) && sdy != 0) dy = 0, dx = -1;
 
 		if(move_step <= 0.) {
+
+			sdx = dx;
+			sdy = dy;
 
 			//Remove occupation from tail
 			occupied_arena[snek_pieces[snek_len-1].x + snek_pieces[snek_len-1].y * ARENA_W] = 0;
@@ -121,18 +124,23 @@ int main(int argc, char* argv[]) {
 				snek_pieces[i].y = snek_pieces[i - 1].y;
 			}
 
-			snek_pieces[0].x += dx;
-			snek_pieces[0].y += dy;
+			snek_pieces[0].x += sdx;
+			snek_pieces[0].y += sdy;
+
+			
+			if(snek_pieces[0].x > (ARENA_W - 1)) 
+				snek_pieces[0].x = 0;
+			if(snek_pieces[0].y > (ARENA_H - 1)) 
+				snek_pieces[0].y = 0;
+			
+			if(snek_pieces[0].x < 0) 
+				snek_pieces[0].x = (ARENA_W - 1);
+			if(snek_pieces[0].y < 0) 
+				snek_pieces[0].y = (ARENA_H - 1);
 
 			//Add occupation to head
 			occupied_arena[snek_pieces[0].x + snek_pieces[0].y * ARENA_W] = 1;
 			
-			if(snek_pieces[0].x > (ARENA_W - 1)) snek_pieces[0].x = 0;
-			if(snek_pieces[0].y > (ARENA_H - 1)) snek_pieces[0].y = 0;
-			
-			if(snek_pieces[0].x < 0) snek_pieces[0].x = (ARENA_W - 1);
-			if(snek_pieces[0].y < 0) snek_pieces[0].y = (ARENA_H - 1);
-
 			for(uint32_t i = 1; i < snek_len; i++) {
 				if(snek_pieces[0].x == snek_pieces[i].x && snek_pieces[0].y == snek_pieces[i].y) {
 					failed = 1;
@@ -161,7 +169,6 @@ int main(int argc, char* argv[]) {
 				next_step = SPEED_FORMULA;
 				if(next_step < 0.) next_step = 0.;
 
-				printf("Step speed %lf\n", next_step);
 			}
 
 			move_step = next_step;
@@ -172,6 +179,8 @@ int main(int argc, char* argv[]) {
 		}
 
 		olivec_fill(canvas, 0xFF000000AA);
+
+		#if 1
 		olivec_rect(canvas, 0, SCREEN_H-HUD_H, SCREEN_W, HUD_H, 0xFF444444);
 		olivec_rect(canvas, 0, SCREEN_H-HUD_H, SCREEN_W, 3, 0xFF888888);
 
@@ -180,7 +189,7 @@ int main(int argc, char* argv[]) {
 		sprintf(guibuf, "Points: %d", (snek_len - 3));
 		olivec_text_(canvas, guibuf, 10, SCREEN_H-HUD_H+10, smol_font_, 1, 0xFFFFFF00);
 		sprintf(guibuf, "FPS: %d", fps);
-		olivec_text_(canvas, guibuf, 10, SCREEN_H-HUD_H+32, smol_font_, 1, 0xFFFFFF00);
+		olivec_text_(canvas, guibuf, 10, SCREEN_H-HUD_H+26, smol_font_, 1, 0xFFFFFF00);
 
 		for(int i = 0; i < snek_len; i++) {
 			uint32_t col = (128.+cos((double)i * 6.283/5.0)*63.0);
@@ -188,9 +197,18 @@ int main(int argc, char* argv[]) {
 			col |= 0xFF000000;
 			olivec_rect(canvas, snek_pieces[i].x*SNAKE_SIZE, snek_pieces[i].y*SNAKE_SIZE, SNAKE_SIZE, SNAKE_SIZE, i == 0 ? 0xFFAACCFF : col);
 		}
+		#else 
+		
+		//Debug occupied map.
+		for(int j = 0; j < ARENA_H; j++)
+		for(int i = 0; i < ARENA_W; i++)
+		{
+			olivec_rect(canvas, i * SNAKE_SIZE, j * SNAKE_SIZE, SNAKE_SIZE, SNAKE_SIZE, occupied_arena[i + j * ARENA_W] ? 0xFFFFFFFF : 0xFF000000);
+		}
 
-	
-		smol_frame_blit_pixels(frame, pixels[0], SCREEN_W, SCREEN_H, 0, 0, SCREEN_W, SCREEN_H, 0, 0, SCREEN_W, SCREEN_H);
+		#endif 
+
+		smol_frame_blit_pixels(frame, pixels[0], SCREEN_W, SCREEN_H, 0, 0, SCREEN_W * PIXEL_SCALE, SCREEN_H * PIXEL_SCALE, 0, 0, SCREEN_W, SCREEN_H);
 
 		dt = smol_timer() - tp1;
 		time_accum += dt;
