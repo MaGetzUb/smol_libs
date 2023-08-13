1f3d026b1ba4ddb213d3c34b73e87e932d0b95ce
Marko <marko_ranta@protonmail.com>
2023-08-13 03:08:29 +0300

    PixFont creator can now generate also c-blobs that contain base64-encoded font data.

--- smol_pix_font_creator.c
+++ smol_pix_font_creator.c
@@ -10,10 +10,15 @@
 #define SMOL_INPUT_IMPLEMENTATION
 #include "smol_input.h"
 
+#define SMOL_CANVAS_IMPLEMENTATION
+#include "smol_canvas.h"
+
 #include "thirdparty/tinyfiledialogs.h"
 
 //I build this file on Windows with these arguments:
 //cl.exe /Zi /EHsc /nologo /Fo:.\build\ .\smol_pix_font_creator.c /link kernel32.lib user32.lib comdlg32.lib ole32.lib shell32.lib /OUT:.\build\smol_pix_font_creator.exe
+int num_chars = 0;
+int char_w, char_h;
 
 char* pix_buffer = NULL;
 char* offsets[128] = {0};
@@ -21,23 +26,39 @@ char* indexes = NULL;
 char rev_indexes[128];
 char char_geometry[128][2];
 
-int num_chars = 0;
-int char_w, char_h;
 
 void load_font(const char* path);
 void save_font(const char* path);
 void export_c_header(const char* path);
+void export_c_blob(const char* path);
+
+BOOL SaveBitmapToFile(const char* filename, HBITMAP hBitmap);
+
+#ifdef UNICODE
+#	define tprintf wprintf
+#else 
+#	define tprintf printf
+#endif 
+#define QUICK_TEST 1
 
 int main() {
+
+
 
 	int stage = 0;
 	int index = 0;
 
 	puts("Type 1 (enter) to create a new font.");
 	puts("Type 2 (enter) to load an existing font.");
+#if _WIN32
+	puts("Type 3 (enter) to create font from Windows fonts.");
+#endif
 	printf("How would you like to proceed: ");
+#if !QUICK_TEST
 	scanf("%d", &stage);
-
+#else
+	stage = 3;
+#endif 
 	if(stage == 1) {
 		puts("Max character size is 32x32");
 
@@ -81,16 +102,230 @@ int main() {
 		load_font(path);
 
 	}
+#if _WIN32
+	else if(stage == 3) {
+
+		char buf[128] = { 0 };
+		char pxf_buf[128] = { 0 };
+		char header_buf[128] = { 0 };
+		int chars = 0;
+		
+		int font_loaded = 0;
+		while(!font_loaded) {
+
+		#if !QUICK_TEST
+			printf("Type in type-face name or font file name: ");
+			scanf("%s", buf);
+			chars = strlen(buf);
+
+
+			printf("Char size: ");
+			scanf("%d", &char_w);
+
+			while(char_w > 32) {
+				puts("Pick a width that's less  than or equal to 32 pixels!");
+				printf("Char width: ");
+				if(scanf("%d", &char_w) == 0) exit(0);
+			}
+
+			char_h = char_w;
+
+			printf("Char size will be %d x %d pixels\n", char_w, char_h);
+		#else 
+			strcpy(buf, "Courier");
+			chars = 7;
+			char_h = char_w = 12;
+		#endif 
+		
+		#define CHARS_PER_SIDE 16
+
+			//NONANTIALIASED_QUALITY
+			int last_error = GetLastError();
+	#ifdef UNICODE
+			TCHAR typeface[128] = { 0 };
+			MultiByteToWideChar(CP_UTF8, 0, buf, chars, typeface, 128);
+	#else
+			TCHAR* typeface = buf;
+	#endif 
+			HDC hdcScreen = GetDC(NULL);
+			
+
+			// Set the desired font attributes (not rotated)
+			LOGFONT lf = { 0 };
+			
+			lf.lfHeight = char_h; // Font height
+			lf.lfWeight = FW_THIN; // Font weight
+			lf.lfCharSet = DEFAULT_CHARSET; // Character set
+			lf.lfQuality = NONANTIALIASED_QUALITY;
+			//tstrcpy_s(lf.lfFaceName, LF_FACESIZE, typeface); // Font face name
+			memcpy(lf.lfFaceName, typeface, LF_FACESIZE * sizeof(TCHAR));
+
+			// Create the font
+			HFONT font = CreateFontIndirect(&lf);
+
+
+
+			last_error = GetLastError();
+			if(last_error) {
+				TCHAR err_buf[512] = { 0 };
+				FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, 0, last_error, 0, err_buf, 511, NULL);
+				tprintf(TEXT("%s\n"), err_buf);
+
+				printf("FAILED to load font '%s'\n", buf);
+				continue;
+			}
+			font_loaded = 1;
+
+			// Create a device context
+			HDC hdcBmp = CreateCompatibleDC(hdcScreen);
+
+			
+			HFONT old = (HFONT)SelectObject(hdcBmp, font);
+
+			
+			int cw = 0;
+			int ch = 0;
+			for(int i = 0; i < 128; i++) {
+				char text[1] = { i };
+				RECT bounds = { 0 };
+				GetTextExtentPoint32(hdcBmp, text, 1, &bounds);
+				int w = abs(bounds.right - bounds.left);
+				int h = abs(bounds.bottom - bounds.top);
+				if(w > cw) cw = w;
+				if(h > ch) ch = h;
+			}
+			char_w = cw;
+			char_h = ch;
+
+			int atlas_w = CHARS_PER_SIDE * char_w;
+			int atlas_h = (CHARS_PER_SIDE / 2) * char_h;
+
+			BITMAPINFO bmpInfo = { 0 };
+			bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+			bmpInfo.bmiHeader.biWidth = atlas_w;
+			bmpInfo.bmiHeader.biHeight = -atlas_h; // negative height for a top-down bitmap (origin at top-left)
+			bmpInfo.bmiHeader.biPlanes = 1;
+			bmpInfo.bmiHeader.biBitCount = 32; // 32 bits per pixel (RGBA)
+			bmpInfo.bmiHeader.biCompression = BI_RGB;
+
+			// Create the bitmap and select it into the compatible DC
+			void* pBits;
+			HBITMAP hBitmap = CreateDIBSection(hdcBmp, &bmpInfo, DIB_RGB_COLORS, &pBits, NULL, 0);
+
+			HBITMAP hbmOld = (HBITMAP)SelectObject(hdcBmp, hBitmap);
+
+
+
+			RECT rect = { 0, 0, atlas_w, atlas_h };
+			FillRect(hdcBmp, &rect, GetStockObject(BLACK_BRUSH));
+			SetBkMode(hdcBmp, TRANSPARENT);
+			SelectObject(hdcBmp, GetStockObject(WHITE_BRUSH));
+			COLORREF colorRef = RGB(255, 255, 255);
+			SetTextColor(hdcBmp, colorRef);
+			//SetTextAlign(hdcBmp, TA_CENTER | TA_TOP);
+			
+			for(int i = 0; i < 128; i++) {
+				if(isprint(i) == 0 || isspace(i))
+					continue;
+				TCHAR text[1] = { i };
+
+				RECT rect = { 0 };
+				rect.left = ((i % CHARS_PER_SIDE) * char_w);
+				rect.top =  ((i / CHARS_PER_SIDE) * char_h);
+				rect.right  = (rect.left + char_w);
+				rect.bottom = (rect.top  + char_h);
+				
+				RECT bounds = { 0 };
+				GetTextExtentPoint32(hdcBmp, text, 1, &bounds);
+				//printf("(%d, %d)-(%d, %d) [%d x %d]\n", bounds.left, bounds.top, bounds.right, bounds.bottom, bounds.right - bounds.left, bounds.bottom - bounds.top);
+
+				int bw = abs(bounds.right - bounds.left);
+
+				//TextOut(hdcBmp, rect.left, rect.top, text, 1);
+
+				if(DrawTextA(hdcBmp, text, 1, &rect, DT_SINGLELINE | DT_CENTER) == 0) {
+					printf("Error drawing text!");
+				}
+				num_chars++;
+			}
+
+			BITMAP bitmap = { 0 };
+			GetObject(hBitmap, sizeof(BITMAP), &bitmap);
+
+
+			pix_buffer = malloc(char_w * char_h * num_chars);
+			indexes = malloc(num_chars);
+			memset(offsets, 0, sizeof(offsets));
+			memset(pix_buffer, 0, char_w * char_h * num_chars);
+			memset(indexes, 0, num_chars);	
+
+			UINT32* pixels = (UINT32*)bitmap.bmBits;
+	
+			for(int i = 0; i < 128; i++) {
+
+				if(isprint(i) == 0 || isspace(i))
+					continue;
+
+				
+				int x = (i % CHARS_PER_SIDE) * char_w;
+				int y = (i / CHARS_PER_SIDE) * char_h;
+
+				for(int py = 0; py < char_h; py++)
+				for(int px = 0; px < char_w; px++)
+				{
+					int p = (pixels[(x + px) + (y + py) * atlas_w] & 0xFF);
+					pix_buffer[(index * char_w * char_h) + (px + py * char_w)] = (p != 0);
+				}
+
+				index++;
+			}
+
+
+
+
+			int file_ext_starts = -1;
+			int file_name_starts = 0;
+			for(UINT32 i = chars - 1; i < chars; i--) {
+				if(buf[i] == '.') {
+					file_ext_starts = i;
+				}
+				if(buf[i] == '\\' || buf[i] == '/') {
+					file_name_starts = i;
+					break;
+				}
+			}
+			
+
+			int nchars_to_write = file_ext_starts - file_name_starts;
+			
+
+
+			//memcpy(pxf_buf, buf, nchars_to_write);
+			//strcat(pxf_buf, ".pxf");
+			//save_font(pxf_buf);
+
+			//memcpy(header_buf, buf, nchars_to_write);
+			//strcat(header_buf, ".h");
+			//export_c_header(header_buf);
+			SaveBitmapToFile("test.bmp", hBitmap);
+		
+			SelectObject(hdcBmp, hbmOld);
+			DeleteDC(hdcBmp);
+			ReleaseDC(NULL, hdcScreen);
+		}
+
+	}
+#endif 
 	else {
 		exit(0);
 	}
 
 
-
+	index = 0;
 	for(int i = 0; i < 128; i++) {
 		if(isprint(i) && !isspace(i)) {
 			indexes[index] = (char)i;
-			offsets[i] = &pix_buffer[index*char_w*char_h];
+			offsets[i] = &pix_buffer[index * char_w * char_h];
 			rev_indexes[i] = index;
 			index++;
 		}
@@ -106,11 +341,22 @@ int main() {
 		"{|}~"
 	};
 
+#ifdef _WIN32
+	int h = GetSystemMetrics(SM_CYSCREEN);
+	int w = GetSystemMetrics(SM_CXSCREEN);
+#else
+	int w = 1920;
+	int h = 720;
+#endif  
 	//Calculate ideal window size based on the character size
 	int scale = 20;
 	int cmax = (char_w > char_h ? char_w : char_h);
-	while(cmax * scale < 640)
+	int limit = (w > h) ? h : w;
+	limit = (limit * 3)/4;
+
+	while((cmax * (scale*2)) < limit)
 		scale *= 2;
+
 
 	int width = char_w * scale;
 	int height = char_h * scale + char_h*5;
@@ -132,6 +378,7 @@ int main() {
 	puts("F5 = Save a pixel font file (.pfx)");
 	puts("F6 = Open a pixel font file.");
 	puts("F7 = Export a C header file. (.h)");
+	puts("F8 = Export as C data blob. (const char[])");
 	puts("Place pixels by pressimg LMB");
 	puts("Erase pixels by pressing RMB");
 	puts("Change to next character Right Arrow Key, or Mouse Wheel Up.");
@@ -240,6 +487,12 @@ int main() {
 			if(path) export_c_header(path);
 		}
 
+		if(smol_key_hit(SMOLK_F8)) {
+			const char* exts[] = { "*.h", "*.c"};
+			char* path = tinyfd_saveFileDialog("Export C array", "", sizeof(exts)/sizeof(*exts), exts, NULL);
+			if(path) export_c_blob(path);
+		}
+
 		if(cur_char < 0) cur_char = 0;
 		if(cur_char > (num_chars-1)) cur_char = (num_chars-1);
 
@@ -249,7 +502,7 @@ int main() {
 		//	FB_PIX_AT(x, y*scale) = 0xFF444444;
 		//}
 
-		//Drag hor grid
+		//Draw hor grid
 		for(int j = 0; j < char_h+1; j++)
 		for(int i = 0; i < width; i++) {
 			FB_PIX_AT(i, j*scale) = 0xFF888888;
@@ -263,6 +516,13 @@ int main() {
 			FB_PIX_AT(i*scale, j) = 0xFF888888;
 		}
 
+		for(int y = 0; y < char_h*scale; y++) {
+			int x = char_geometry[indexes[cur_char]][0]*scale;
+			int w = char_geometry[indexes[cur_char]][1]*scale;
+			FB_PIX_AT(x, y) = 0xFFFFFF00;
+			FB_PIX_AT(x+w, y) = 0xFFFFFF00;
+		
+		}
 
 
 		for(int y = 0; y < char_h; y++)
@@ -323,6 +583,8 @@ int main() {
 
 
 		smol_frame_blit_pixels(frame, frame_buffer, width, height, 0, 0, width, height, 0, 0, width, height);
+
+		
 
 		if(prev_char != cur_char) {
 			sprintf(title_buf, "PixFont Creator - Current Character: %c", indexes[cur_char]);
@@ -400,7 +662,35 @@ void load_font(const char* path) {
 
 }
 
+void update_bounds() {
+	#define CHAR_PIX_AT(px, py) offsets[indexes[i]][(px) + (py)*char_w]
+
+	for(int i = 0; i < num_chars; i++) {
+
+		int minX = 32;
+		int maxX = 0;
+
+		for(int y = 0; y < char_h; y++)
+		for(int x = 0; x < char_w; x++) {
+			char ch = (CHAR_PIX_AT(x, y) ? '1' : '0');
+			if(ch == '1') {
+				if(x < minX) minX = x;
+				if(x > maxX) maxX = x;
+			}
+
+			if(maxX >= minX) {
+				char_geometry[indexes[i]][0] = minX;
+				char_geometry[indexes[i]][1] = (maxX - minX) + 1;
+			}
+		}
+		
+	}
+	#undef CHAR_PIX_AT
+}
+
 void save_font(const char* path) {
+
+	update_bounds();
 
 	FILE* file = fopen(path, "w");
 	fprintf(file, "glyph_size: %d %d\n", char_w, char_h);
@@ -420,17 +710,9 @@ void save_font(const char* path) {
 		for(int y = 0; y < char_h; y++) 
 		for(int x = 0; x < char_w; x++) {
 			char ch = (CHAR_PIX_AT(x, y) ? '1' : '0');
-			if(ch == '1') {
-				if(x < minX) minX = x;
-				if(x > maxX) maxX = x;
-			}
 			fprintf(file, "%c%s", ch, next[(x + 1) >= char_w]);
-
-			if(maxX > minX) {
-				char_geometry[indexes[i]][0] = minX;
-				char_geometry[indexes[i]][1] = (maxX - minX);
-			}
 		}
+		//printf("%c [p: %d w: %d]\n", indexes[i], minX, (maxX - minX));
 		
 	}
 	#undef CHAR_PIX_AT
@@ -448,6 +730,8 @@ void export_c_header(const char* path) {
 	char buf[2048];
 	strcpy(buf, path);
 	
+	update_bounds();
+
 	char* file_name = NULL;
 	char* font_name = NULL;
 	char* tok = strtok(buf, "\\/");
@@ -495,20 +779,27 @@ void export_c_header(const char* path) {
 	#define CHAR_PIX_AT(px, py) offsets[i][(px) + (py)*char_w]
 	for(int i = 0; i < 128; i++) {
 		if(offsets[i]) {
+
+
 			fprintf(file, "\t{/* %c */\n", i);
 			for(int y = 0; y < char_h; y++) {
 				fprintf(file, "\t\t{");
 				for(int x = 0; x < char_w; x++) {
 					char ch = (CHAR_PIX_AT(x, y) ? '1' : '0');
 					fprintf(file, "%c%s", ch, next[(x + 1) >= char_w]);
+					//printf("%c", ch);
 				}
 				fprintf(file, "%s", next_char[(y+1) >= char_h]);
+				//puts("");
 			}
+
+			//printf("%c [p: %d w: %d]\n", i, minX, (maxX - minX));
 			fprintf(file, "\t},\n");
 		}
 		else {
 			fprintf(file, "\t{0} /* 0x%x */ %s", i, next_char[(i+1) >= 128]);
 		}
+
 	}
 	#endif 
 
@@ -541,8 +832,180 @@ void export_c_header(const char* path) {
 	#undef CHAR_PIX_AT
 
 	fclose(file);
+}
+
+void export_c_blob(const char* path) {
+
+	char buf[2048];
+	strcpy(buf, path);
+
+	update_bounds();
+
+	char* file_name = NULL;
+	char* font_name = NULL;
+	char* tok = strtok(buf, "\\/");
+	for(; tok != NULL;) {
+		file_name = tok;
+		tok = strtok(NULL, "\\/");
+	}
+
+	font_name = strtok(file_name, ".");
+	for(int i = 0; i < strlen(font_name); i++)
+		font_name[i] = toupper(font_name[i]);
+
+	int size = (num_chars * char_w * char_h);
+	int total_size = (size / 8) + (num_chars * sizeof(char) * 2);
+	char* font_buf = (char*)malloc(total_size);
+	memset(font_buf, 0, total_size);
+	char* geom_buf = (char*)malloc(num_chars * 2);
+	memset(geom_buf, 0, num_chars * 2);
+	int npixels = char_w * char_h;
+
+	int font_idx = 0;
+	int idx = 0;
+	for(int i = 0; i < num_chars; i++) {
+
+		const char* buf_ptr = &pix_buffer[i * npixels];
+
+		for(int y = 0; y < char_h; y++)
+			for(int x = 0; x < char_w; x++)
+			{
+				if(buf_ptr[x + y * char_w]) {
+					font_buf[idx / 8] |= (0x80 >> (idx % 8));
+				}
+				font_idx += (idx && (idx % 8) == 0);
+				idx++;
+			}
+		//printf("%c at index: %d saved.\n", 33+i, i*char_w*char_h);
+	}
+
+
+	int geom_index = 0;
+	for(int i = 0; i < num_chars; i++) {
+		geom_buf[geom_index++] = char_geometry[33 + i][0];
+		geom_buf[geom_index++] = char_geometry[33 + i][1];
+	}
+
+	const char b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+
+	FILE* file = fopen(path, "w");
+	fprintf(file, "#define PXF_%s_WIDTH %d\n", font_name, char_w);
+	fprintf(file, "#define PXF_%s_HEIGHT %d\n", font_name, char_h);
+	fprintf(file, "const char PXF_%s_FONT_BLOB[] = \n\"", font_name);
+	int i = 0;
+	for(; i < font_idx; i += 3) {
+		unsigned int b24 = (
+			(unsigned char)(font_buf[i + 0]) << 16 |
+			(unsigned char)(font_buf[i + 1]) << 8 |
+			(unsigned char)(font_buf[i + 2]) << 0
+		);
+
+		char a = b64[(b24 >> 0x12) & 0x3FU];
+		char b = b64[(b24 >> 0x0C) & 0x3FU];
+		char c = b64[(b24 >> 0x06) & 0x3FU];
+		char d = b64[(b24 >> 0x00) & 0x3FU];
+
+		fprintf(file, "%c%c%c%c", a, b, c, d);
+		if(i && i % (32 * 3) == 0) fprintf(file, "\"\n\"");
+	}
+
+	{
+		int padding = size % 3;
+		if(padding > 0) padding = 3 - padding;
+		fprintf(file, "%.*s", padding + 1, "====");
+	}
+	fprintf(file, "\";\n");
+	//
+	fprintf(file, "const char* PXF_%s_GEOM_BLOB = \n\"", font_name);
+	for(i = 0; i < geom_index; i += 3) {
+		unsigned int b24 = (
+			(unsigned char)(geom_buf[i + 0]) << 16 |
+			(unsigned char)(geom_buf[i + 1]) << 8 |
+			(unsigned char)(geom_buf[i + 2]) << 0
+		);
+
+		char a = b64[(b24 >> 0x12) & 0x3FU];
+		char b = b64[(b24 >> 0x0C) & 0x3FU];
+		char c = b64[(b24 >> 0x06) & 0x3FU];
+		char d = b64[(b24 >> 0x00) & 0x3FU];
+
+		fprintf(file, "%c%c%c%c", a, b, c, d);
+		if(i && i % (32 * 3) == 0) fprintf(file, "\"\n\"");
+	}
+	{
+		int padding = size % 3;
+		if(padding > 0) padding = 3 - padding;
+		fprintf(file, "%.*s", padding + 1, "====");
+	}
+	fprintf(file, "\";");
+	/*fprintf(file, "const char PXF_%s_DATA_BLOB[] = {\n\t", font_name);
+	for(int i = 0; i < size/8; i++) {
+		fprintf(file,"%u,", (data_buf[i] & 0xFFU));
+		if(i && (i % 32) == 0) fprintf(file, "\n\t");
+	}
+	fprintf(file, "\n};");*/
+	fclose(file);
 
 }
 
- 
+
+BOOL SaveBitmapToFile(const char* filename, HBITMAP hBitmap) {
+	BITMAP bitmap;
+	GetObject(hBitmap, sizeof(BITMAP), &bitmap);
+
+	BITMAPFILEHEADER bmfHeader;
+
+	BYTE byte[sizeof(BITMAPINFOHEADER) /*+ 3*sizeof(UINT32)*/];
+
+	BITMAPINFOHEADER* bi = byte;
+
+	bi->biSize = sizeof(BITMAPINFOHEADER);
+	bi->biWidth = bitmap.bmWidth;
+	bi->biHeight = -bitmap.bmHeight;
+	bi->biPlanes = 1;
+	bi->biBitCount = bitmap.bmBitsPixel;
+	bi->biCompression = BI_RGB;
+	bi->biSizeImage = 0;
+	bi->biXPelsPerMeter = 0;
+	bi->biYPelsPerMeter = 0;
+	bi->biClrUsed = 0;
+	bi->biClrImportant = 0;
+
+	//UINT32(*rgb)[3] = (UINT32*)&byte[sizeof(BITMAPINFOHEADER)];
+	//rgb[0][0] = 0xFF0000;
+	//rgb[0][1] = 0x00FF00;
+	//rgb[0][2] = 0x0000FF;
+
+	DWORD dwBmpSize = ((bitmap.bmWidth * bi->biBitCount + 31) / 32) * 4 * bitmap.bmHeight;
+
+#ifndef UNICODE
+	TCHAR* filepath = filename;
+#else 
+	TCHAR filepath[512] = { 0 };
+	MultiByteToWideChar(CP_UTF8, 0, filename, strlen(filename), filepath, 511);
+#endif
+
+	HANDLE hFile = CreateFile(filepath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	
+	if (hFile == INVALID_HANDLE_VALUE) {
+		return FALSE;
+	}
+
+	DWORD dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+	bmfHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);
+	bmfHeader.bfSize = dwSizeofDIB;
+	bmfHeader.bfType = 0x4D42; // BM
+
+	DWORD dwBytesWritten;
+	WriteFile(hFile, (LPSTR)&bmfHeader, sizeof(BITMAPFILEHEADER), &dwBytesWritten, NULL);
+	WriteFile(hFile, (LPSTR)byte, sizeof(byte), &dwBytesWritten, NULL);
+	WriteFile(hFile, (LPSTR)bitmap.bmBits, dwBmpSize, &dwBytesWritten, NULL);
+
+	CloseHandle(hFile);
+
+	return TRUE;
+}
+
 #include "thirdparty/tinyfiledialogs.c"
